5251d48cca9736d9d87fe88ba15cd6df66bfd12e
seprog <sebastianpoppenborg@gmail.com>
2025-09-30 17:11:52 +0200

    fixed cd after paru install

--- steps/01-base/01-install/setup.sh
+++ steps/01-base/01-install/setup.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 
+SETUP_PATH=$PWD
+
 if ! type "paru" > /dev/null; then
   sudo pacman --sync --needed --noconfirm base-devel
   [[ -d /tmp/paru ]] && sudo rm --recursive /tmp/paru
   git clone https://aur.archlinux.org/paru.git /tmp/paru
   cd /tmp/paru
   makepkg --syncdeps --install --needed --noconfirm
-  cd ..
+  cd $SETUP_PATH
   sudo rm --recursive /tmp/paru
 fi
 
-paru -S --needed --noconfirm $(grep "^[^#]" packages)
+paru -S --needed --noconfirm $(grep "^[^#]" $SETUP_PATH/packages)
