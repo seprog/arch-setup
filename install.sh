81067e96dfd0a10b31980c3da34aa77249a9bcc6
seprog <sebastianpoppenborg@gmail.com>
2025-09-30 17:33:32 +0200

    changed setup_file variable to full path

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 
 shopt -s globstar
-RETURN_PATH=$PWD
+SETUP_PATH=$PWD
 cd steps
-for setup_file in **/setup.sh; do 
+for setup_file in $SETUP_PATH/**/setup.sh; do 
   read -r -p $'Execute step \e[1m'"$(dirname "$setup_file")"$'\e[0m? (y/N) ' execute
   [[ $execute == [Yy]* ]] && cd $(dirname "$setup_file") && bash setup.sh
 done
-cd $RETURN_PATH
+cd $SETUP_PATH
 
 # cd .. && rm -R arch-setup
