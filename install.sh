c01a5f1849555926a044e56336315d5ddeedc212
seprog <sebastianpoppenborg@gmail.com>
2025-09-30 16:28:24 +0200

    fixed setup.sh invocation

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ RETURN_PATH=$PWD
 cd steps
 for setup_file in **/setup.sh; do 
   read -r -p $'Execute step \e[1m'"$(dirname "$setup_file")"$'\e[0m? (y/N) ' execute
-  [[ $execute == [Yy]* ]] && cd $(dirname "$setup_file") && bash $setup_file
+  [[ $execute == [Yy]* ]] && cd $(dirname "$setup_file") && bash setup.sh
 done
 cd $RETURN_PATH
 
