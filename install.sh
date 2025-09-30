1366fac7fdc64b7a1b6ef1178c95a7028b9be912
seprog <sebastianpoppenborg@gmail.com>
2025-09-30 18:27:14 +0200

    refactor

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
 
-shopt -s globstar
 SETUP_PATH=$PWD
-cd steps
-for setup_file in $SETUP_PATH/**/setup.sh; do 
-  read -r -p $'Execute step \e[1m'"$(dirname "$setup_file")"$'\e[0m? (y/N) ' execute
-  [[ $execute == [Yy]* ]] && cd $(dirname "$setup_file") && bash setup.sh
-done
-cd $SETUP_PATH
+
+bash steps/setup.sh
 
 # cd .. && rm -R arch-setup
---
+++ steps/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+shopt -s globstar
+SETUP_PATH=$PWD
+for setup_file in **/setup.sh; do 
+  read -r -p $'Execute step \e[1m'$(dirname $setup_file)$'\e[0m? (y/N) ' execute
+  [[ $execute == [Yy]* ]] && cd $SETUP_PATH/$(dirname $setup_file) && bash setup.sh
+done
+cd $SETUP_PATH
+
+# cd .. && rm -R arch-setup
