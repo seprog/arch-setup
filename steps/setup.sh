3fefb9f723ea1dacc1edf7b3e5719b4b64e2823e
seprog <sebastianpoppenborg@gmail.com>
2025-09-30 18:55:56 +0200

    fixed steps.sh recursive execution logic

--- steps/setup.sh
+++ steps/setup.sh
@@ -3,7 +3,7 @@
 
 shopt -s globstar
 SETUP_PATH=$PWD
-for setup_file in **/setup.sh; do
+for setup_file in */**/*/setup.sh; do
   read -r -p $'Execute step \e[1m'$(dirname $setup_file)$'\e[0m? (y/N) ' execute
   [[ $execute == [Yy]* ]] && cd $SETUP_PATH/$(dirname $setup_file) && bash setup.sh
 done
