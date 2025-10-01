d94e7bbbe505c8825ed3ee862cf964701de7a2ae
seprog <sebastianpoppenborg@gmail.com>
2025-10-01 22:58:00 +0200

    testing sddm

--- steps/01-base/02-config/setup.sh
+++ steps/01-base/02-config/setup.sh
@@ -5,5 +5,6 @@ SETUP_PATH=$PWD
 
 cp --recursive $SETUP_PATH/home/. ~
 
+sudo systemctl enable sddm.service
 systemctl --user enable --now hyprpolkitagent.service
 systemctl --user enable --now foot-server.service
