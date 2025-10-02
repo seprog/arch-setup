84f4a0b59f1924c0685a3420b8671f123ecede81
seprog <sebastianpoppenborg@gmail.com>
2025-10-02 04:58:04 +0200

    refactor

--- steps/01-base/02-config/setup.sh
+++ steps/01-base/02-config/setup.sh
@@ -5,8 +5,8 @@ SETUP_PATH=$PWD
 
 cp --recursive $SETUP_PATH/home/. ~
 
-sudo systemctl enable sddm.service
-sudo systemctl enable NetworkManager.service
+sudo systemctl enable --now NetworkManager.service
+sudo systemctl enable --now sddm.service
+systemctl --user enable --now foot-server.service
 systemctl --user enable --now hyprpolkitagent.service
 systemctl --user enable --now waybar.service
-systemctl --user enable --now foot-server.service
