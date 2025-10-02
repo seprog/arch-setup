13a2342ac9d70b81fd4bcc49aaf10f3db46d5694
seprog <sebastianpoppenborg@gmail.com>
2025-10-02 04:57:16 +0200

    waybar now starts as a service

--- steps/01-base/02-config/setup.sh
+++ steps/01-base/02-config/setup.sh
@@ -8,4 +8,5 @@ cp --recursive $SETUP_PATH/home/. ~
 sudo systemctl enable sddm.service
 sudo systemctl enable NetworkManager.service
 systemctl --user enable --now hyprpolkitagent.service
+systemctl --user enable --now waybar.service
 systemctl --user enable --now foot-server.service
