50c90114f9dfdd3ab5582e95dddde6ab62a5f077
seprog <sebastianpoppenborg@gmail.com>
2025-10-03 03:05:35 +0200

    fixed lla alias

--- steps/01-base/02-config/home/.config/fish/conf.d/aliases.fish
+++ steps/01-base/02-config/home/.config/fish/conf.d/aliases.fish
@@ -2,7 +2,7 @@
 alias ls  'eza      --color=always --icons --group-directories-first'               # default
 alias la  'eza -a   --color=always --icons --group-directories-first'               # hidden
 alias ll  'eza -l   --color=always --icons --group-directories-first --total-size'  # long
-alias ll  'eza -la  --color=always --icons --group-directories-first --total-size'  # long, hidden
+alias lla 'eza -la  --color=always --icons --group-directories-first --total-size'  # long, hidden
 alias lt  'eza -laT --color=always --icons --group-directories-first --total-size'  # long, hidden, tree
 
 # colorize commands
