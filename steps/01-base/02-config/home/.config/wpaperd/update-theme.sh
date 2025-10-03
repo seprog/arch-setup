c675852ea64f9fad1e2fd9e75982935a849bfcd0
seprog <sebastianpoppenborg@gmail.com>
2025-10-03 10:24:56 +0200

    reduced palette size from 4 to 3

--- steps/01-base/02-config/home/.config/wpaperd/update-theme.sh
+++ steps/01-base/02-config/home/.config/wpaperd/update-theme.sh
@@ -4,6 +4,6 @@
 monitor=$1
 wallpaper=$2
 
-wallrust --wallset --html --output-dir $XDG_STATE_HOME/dynamic-theme/active $2
+wallrust --wallset --html --colors 3 --output-dir $XDG_STATE_HOME/dynamic-theme/active $2
 
 systemctl --user reload-or-restart waybar.service
