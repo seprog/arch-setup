9cf3cfe44cf971690b495cc50552dd5d8d29a788
seprog <sebastianpoppenborg@gmail.com>
2025-10-04 04:14:48 +0200

    changed wallrust output folder

--- steps/01-base/02-config/home/.config/wallrust/update-theme.sh
+++ steps/01-base/02-config/home/.config/wallrust/update-theme.sh
@@ -4,7 +4,11 @@
 monitor=$1
 wallpaper=$2
 
-wallrust --wallset --html --colors 3 --output-dir $XDG_STATE_HOME/dynamic-theme/active $2
+# generate palette of wallpaper and write configs
+wallrust --wallset --html --colors 3 --output-dir $XDG_STATE_HOME/wallrust/$monitor $wallpaper
 
+# reload applications
+# hyprland reloads automatically
+# hyprlock reloads automatically
 systemctl --user reload-or-restart waybar.service
 killall -SIGUSR1 kitty
