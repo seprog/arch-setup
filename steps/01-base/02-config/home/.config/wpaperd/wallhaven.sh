4db09f13096abd453f5d9427521af827e352e4d6
seprog <sebastianpoppenborg@gmail.com>
2025-10-09 00:40:37 +0200

    removed unnecessary escape characters

--- steps/01-base/02-config/home/.config/wpaperd/wallhaven.sh
+++ steps/01-base/02-config/home/.config/wpaperd/wallhaven.sh
@@ -41,7 +41,7 @@ do
 
   # MAIN COMMAND
   curl --silent --fail --location --header "X-API-Key: $wh_api_key" "$wh_url" | \
-  grep --only-matching --perl-regexp '\"path\":\"\K[^"]+' | \
+  grep --only-matching --perl-regexp '"path":"\K[^"]+' | \
   xargs -P 8 \
     wget --quiet --show-progress --no-clobber --directory-prefix $output_dir
 
