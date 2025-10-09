2cc9a27630d7edca180ffb21aed9d657ae3c3607
seprog <sebastianpoppenborg@gmail.com>
2025-10-09 21:00:00 +0200

    fixed typo

--- steps/01-base/02-config/home/.config/hygge/scripts/post-wallpaper-change.sh
+++ steps/01-base/02-config/home/.config/hygge/scripts/post-wallpaper-change.sh
@@ -18,4 +18,4 @@ bash $HOME/.config/hygge/scripts/generate-theme.sh $wallpaper $wallrust_output_d
 bash $HOME/.config/hygge/scripts/generate-configs.sh $wallrust_output_dir/wallrust.json
 bash $HOME/.config/hygge/scripts/reload-configs.sh
 
-notify-send -u low "Curren Wallpaper" "$(wpaperctl get-all)"
+notify-send -u low "Current Wallpaper" "$(wpaperctl get-all)"
