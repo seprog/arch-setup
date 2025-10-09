0d744dbacab008ca3a9bcd7b9b0b868c3a5d1de2
seprog <sebastianpoppenborg@gmail.com>
2025-10-09 08:41:58 +0200

    reformat

--- steps/01-base/02-config/home/.config/hygge/scripts/update-theme.sh
+++ steps/01-base/02-config/home/.config/hygge/scripts/update-theme.sh
@@ -4,18 +4,24 @@
 monitor=$1
 wallpaper=$2
 
+
 wallrust_output_dir=$XDG_STATE_HOME/hygge/$monitor
 tera_home=$XDG_CONFIG_HOME/hygge/tera/home
 
+
 shopt -s globstar dotglob
 
-# generate palette of wallpaper and write configs
+# generate palette from wallpaper
 wallrust --html --colors 3 --output-dir $wallrust_output_dir $wallpaper
 
+# write configs
 for tera_file in $tera_home/**/*.tera
 do
   tera_file_stripped=${tera_file%.tera}
-  tera --template $tera_file --out ${tera_file_stripped/$tera_home/$HOME} $wallrust_output_dir/wallrust.json
+  tera \
+    --template $tera_file \
+    --out ${tera_file_stripped/$tera_home/$HOME} \
+    $wallrust_output_dir/wallrust.json
 done
 
 # reload applications
