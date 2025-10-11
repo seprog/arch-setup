bf01b62b4e8dfc16cf2e454a1aa7d30e38fd31d3
seprog <sebastianpoppenborg@gmail.com>
2025-10-11 09:28:50 +0200

    moved initial theme to /tmp

--- steps/01-base/02-config/setup.sh
+++ steps/01-base/02-config/setup.sh
@@ -12,8 +12,8 @@ systemctl --user enable hyprpolkitagent.service
 systemctl --user enable waybar.service
 
 bash ~/.config/hygge/scripts/wallhaven.sh
-bash ~/.config/hygge/scripts/generate-theme.sh $SETUP_PATH/wallrust-gradient.svg ~/.local/state/hygge/theme/initial
-bash ~/.config/hygge/scripts/generate-configs.sh ~/.local/state/hygge/theme/initial/wallrust.json
+bash ~/.config/hygge/scripts/generate-theme.sh $SETUP_PATH/wallrust-gradient.svg /tmp/.local/state/hygge/theme/initial
+bash ~/.config/hygge/scripts/generate-configs.sh /tmp/.local/state/hygge/theme/initial/wallrust.json
 
 xdg-user-dirs-update
 sudo chsh -s $(which fish) $USER
