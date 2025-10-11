8454a8837555d1e74b663530cf6ea148683e01d3
seprog <sebastianpoppenborg@gmail.com>
2025-10-11 04:40:48 +0200

    generate themes for all screens

--- steps/01-base/02-config/home/.config/hygge/scripts/post-wallpaper-change.sh
+++ steps/01-base/02-config/home/.config/hygge/scripts/post-wallpaper-change.sh
@@ -8,13 +8,13 @@ wallpaper=$2
 active_monitor="HDMI-A-1"
 
 
-wallrust_output_dir=$HOME/.local/state/hygge/theme/active
-
-
-[ $monitor != $active_monitor ] && exit
+wallrust_output_dir=$HOME/.local/state/hygge/theme/$wallpaper
 
 
 bash $HOME/.config/hygge/scripts/generate-theme.sh $wallpaper $wallrust_output_dir
+
+[ $monitor != $active_monitor ] && exit
+
 bash $HOME/.config/hygge/scripts/generate-configs.sh $wallrust_output_dir/wallrust.json
 bash $HOME/.config/hygge/scripts/reload-configs.sh
 
