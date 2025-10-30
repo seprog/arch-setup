afa1f74226eba79f71825ee7dcf739411b54cef8
seprog <sebastianpoppenborg@gmail.com>
2025-10-30 18:15:09 +0100

    added wallpaper change selection

--- steps/01-base/02-config/home/.config/hygge/scripts/set-wallpaper.sh
+++ steps/01-base/02-config/home/.config/hygge/scripts/set-wallpaper.sh
@@ -8,9 +8,17 @@ theme_dir="$HOME/.local/state/hygge/theme"
 mkdir --parents "$wallpaper_dir"
 
 focused_monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
-[[ $1 == "focused" ]] \
-&& monitors=($focused_monitor) \
-|| monitors=($(hyprctl monitors -j | jq -r '.[].name'))
+if [[ -n "$1" ]]; then
+  if [[ "$1" == "focused" ]]; then
+    monitors=($focused_monitor)
+  elif [[ "$1" == "choose" ]]; then
+    monitors=($(hyprctl monitors -j | jq -r '.[].name' | wofi --dmenu))
+  else
+    monitors=($1)
+  fi
+else
+  monitors=($(hyprctl monitors -j | jq -r '.[].name'))
+fi
 
 # Preload Wallpapers
 # Generate Wallpaper Themes
@@ -20,7 +28,15 @@ for monitor in "${monitors[@]}"; do
   # TODO only find correct file types
   #      (png, jpg, jpeg, jpeg xl, webp)
   #      (accorging to https://github.com/hyprwm/hyprpaper?tab=readme-ov-file#usage)
-  wallpaper=$(find -L "$wallpaper_dir" -type f | shuf -n 1)
+  if [[ -n "$2" ]]; then
+    if [[ "$2" == "choose" ]]; then
+      wallpaper=$(find -L "$wallpaper_dir" -type f | wofi --dmenu)
+    else
+      wallpaper="$2"
+    fi
+  else
+    wallpaper=$(find -L "$wallpaper_dir" -type f | shuf -n 1)
+  fi
   [[ -n "$wallpaper" ]] || exit
   hyprctl hyprpaper preload "$wallpaper"
   bash $scripts_dir/generate-theme.sh $wallpaper $theme_dir/monitors/$monitor
